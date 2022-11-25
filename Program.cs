ab357919160637d269996d05fc2c3995e732dbe0
Acrobaticbag3 <kevintom214@gmail.com>
2022-11-25 09:32:46 +0100

    Crash

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Uppgift1 {
+    class program {
+        public static void Main(string[] args) {
+            int morötterStart = 40;
+            
+            int tidEtt;
+            int tidTvå;
+
+            int morötterÄtna = 0;
+            int timmer = 0;
+
+            Console.Write(value: "Hur lång tid tar Tor på sig att äta en morot: ");
+            tidEtt = Convert.ToInt32(value: Console.ReadLine());
+
+            Console.Write(value: "Hur lång tid tar Mor på sig att äta en morot: ");
+            tidTvå = Convert.ToInt32(value: Console.ReadLine());
+
+            while (morötterStart > 0) {
+                morötterStart--;
+                for (int i = 0; i < tidEtt; i++) {
+                    timmer++;
+                    morötterÄtna++;
+                    morötterStart =- tidEtt;
+                }
+            }
+
+            Console.WriteLine(value: "Tor åt: " + morötterÄtna + " morötter");        
+        }   
+    }
+}
