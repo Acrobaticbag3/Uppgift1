ba684ffe2b9c2a5903ed7b7e619f45d88ec62351
Acrobaticbag3 <kevintom214@gmail.com>
2022-11-25 10:58:09 +0100

    its fine

--- Program.cs
+++ Program.cs
@@ -4,10 +4,13 @@ namespace Uppgift1 {
     class program {
         public static void Main(string[] args) {
             int morötterStart = 40;
+            int time = 100;
+
+            int timerOne = 0;
+            int timerTwo = 0;
 
             int morötterÄtnaFörTor = 0;
             int morötterÄtnaFörMor = 0;
-            int timmer = 0;
 
             Console.Write(value: "Hur lång tid tar Tor på sig att äta en morot: ");
             int tidEtt = Convert.ToInt32(value: Console.ReadLine());
@@ -15,17 +18,23 @@ namespace Uppgift1 {
             Console.Write(value: "Hur lång tid tar Mor på sig att äta en morot: ");
             int tidTvå = Convert.ToInt32(value: Console.ReadLine());
 
-            while (morötterStart != 0) {
-                for (int i = 0; i < tidEtt + 1; i++) {
-                    if (i == tidEtt && morötterStart != 0) {
-                        Console.WriteLine("CALUCLATING");
+            while (morötterStart != 0)
+                for (int i = 0; i < time; i++) {
+                    if (timerOne == tidEtt) {
+
                         morötterÄtnaFörTor++;
-                        i =- tidEtt + 1;
-                        morötterStart--;
+                        timerOne =- tidEtt;
+
+                    } else if (timerTwo == tidTvå) {
+
+                        morötterÄtnaFörMor++;
+                        timerOne =- tidTvå;
+
+                    } else {
+                        timerOne++; 
+                        timerTwo++; 
                     }
                 }
-                morötterStart--;
-            }
 
             Console.WriteLine(value: "Tor åt: " + morötterÄtnaFörTor + " morötter");
             Console.WriteLine(value: "Mor åt: " + morötterÄtnaFörMor + " morötter");        
